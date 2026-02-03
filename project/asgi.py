dc66ba33b906647f2369389ade74f74618ade34f
joykhatri <joykhatri5@gmail.com>
2026-02-03 19:27:21 +0530

    Add files via upload

--- project/asgi.py
+++ project/asgi.py
@@ -11,10 +11,13 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from riders.routing import websocket_urlpatterns
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": URLRouter(websocket_urlpatterns),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
 })
