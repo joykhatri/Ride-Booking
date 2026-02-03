1a6a0ac8b26181b76a991183aaf811136a77847e
joykhatri <joykhatri5@gmail.com>
2026-02-03 14:59:49 +0530

    Update routing.py

--- riders/routing.py
+++ riders/routing.py
@@ -1,10 +1,7 @@
 from django.urls import re_path
 from .consumers import RiderAvailabilityConsumer
 
-# websocket_urlpatterns = [
-#     path("ws/riders/availability/", RiderAvailabilityConsumer.as_asgi()),
-# ]
-
 websocket_urlpatterns = [
     re_path(r"ws/riders/availability/?$", RiderAvailabilityConsumer.as_asgi()),
+
 ]
