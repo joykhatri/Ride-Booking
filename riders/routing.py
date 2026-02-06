acf0fdb61d68fd21bddf5f76e54c39c35c767e35
joykhatri <joykhatri5@gmail.com>
2026-02-06 10:40:29 +0530

    Update routing.py

--- riders/routing.py
+++ riders/routing.py
@@ -4,6 +4,6 @@ from .consumers import RiderAvailabilityConsumer, RideConsumer
 
 websocket_urlpatterns = [
     re_path(r"ws/riders/availability/?$", RiderAvailabilityConsumer.as_asgi()),
-    re_path(r"ws/riders/new_ride/?$", RideConsumer.as_asgi()),
     path(r"ws/riders/new_ride/<int:rider_id>/", RideConsumer.as_asgi()),
+
 ]
