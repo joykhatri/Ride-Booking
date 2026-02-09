ff81958f6e46026f13cb43c873b6d5b62d0ea1fc
joykhatri <joykhatri5@gmail.com>
2026-02-09 18:56:01 +0530

    Add files via upload

--- riders/routing.py
+++ riders/routing.py
@@ -6,5 +6,5 @@ websocket_urlpatterns = [
     re_path(r"ws/riders/availability/?$", RiderAvailabilityConsumer.as_asgi()),
     path(r"ws/riders/new_ride/<int:rider_id>/", RideConsumer.as_asgi()),
     path(r"ws/riders/location/<int:rider_id>/", RiderLocationConsumer.as_asgi()),
-    re_path(r"ws/riders/user_ride/?$", UserRideConsumer.as_asgi()),
+    path(r"ws/riders/user_ride/<int:user_id>/", UserRideConsumer.as_asgi()),
 ]
