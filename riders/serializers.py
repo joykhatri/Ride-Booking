b9ff5ff212d610da54547124b1f227b7a26115aa
joykhatri <joykhatri5@gmail.com>
2026-02-11 18:40:36 +0530

    Update serializers.py

--- riders/serializers.py
+++ riders/serializers.py
@@ -63,8 +63,3 @@ class RiderPaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = RiderPayment
         fields = '__all__'
-
-class RatingsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ratings
-        fields = '__all__'
