cf80d36412f32192ce6582cef1579216bc2463ae
joykhatri <joykhatri5@gmail.com>
2026-02-13 18:07:52 +0530

    Update asgi.py

--- project/asgi.py
+++ project/asgi.py
@@ -12,14 +12,13 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from riders.routing import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
-from riders.middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": JWTAuthMiddleware(AuthMiddlewareStack(
+    "websocket": AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
-        )
-    ),
+        ),
 })
+
