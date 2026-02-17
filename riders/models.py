43b25d92db220045924665ef51a10896b8db996e
joykhatri <joykhatri5@gmail.com>
2026-02-17 17:58:35 +0530

    Update models.py

--- riders/models.py
+++ riders/models.py
@@ -107,10 +107,12 @@ class Ride(models.Model):
 class RiderPayment(models.Model):
     ride = models.OneToOneField(Ride, on_delete=models.CASCADE)
     rider = models.ForeignKey(RiderProfile, on_delete=models.CASCADE)
+    stripe_payment_intent = models.CharField(max_length=250)
+    currency = models.CharField(max_length=10, default="usd")
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    # status = models.CharField(max_length=50, default="pending")
     paid = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Payment for Ride {self.ride.id} - Paid: {self.paid}"
-    
