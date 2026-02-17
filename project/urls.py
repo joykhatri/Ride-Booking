0275cbf6ebd7067d20060b9db229229f9e16d90c
joykhatri <joykhatri5@gmail.com>
2026-02-17 17:57:00 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from riders.webhooks import webhook
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/riders/', include('riders.urls')),
+    path('stripe/webhook/', webhook, name='stripe_webhook')
 ]
